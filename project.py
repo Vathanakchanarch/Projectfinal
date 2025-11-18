c9a9244eba518c881148d7ae96b3948052f9a7bf
Vathanak <archvathanak.chan@student.cadt.edu.kh>
2025-11-18 13:00:38 +0700

    commit

---
+++ project.py
@@ -0,0 +1,12 @@
+import os
+import hashlib
+import magic
+import pyfiglet
+import requests
+
+def display_banner():
+    banner = pyfiglet.figlet_format("CovidVirus")
+    print(banner)
+display_banner()
+
+dfafhafafjdfakjlf
